320070263d622e6b000aaa712848ccfbe08699d4
Aviv Dar <avivzeevdar@gmail.com>
2025-11-09 10:04:48 +0530

    Make script interactive and add error handling

--- tracker.py
+++ tracker.py
@@ -1,15 +1,24 @@
 import yfinance as yf
 
-#Create a "Ticker" object for BHB
-#This opbject represent the stock we want to track
+# Ask the user for a stock ticker
+#the input() function pauses the script and waits for user input
 
-bhb_stock = yf.Ticker("BHB")
+ticker_symbol = input("Enter the stock ticker symbol: ")
 
-#Get the stock's information.
-#info gives us a Python dictionary full of data
+try:
+    #create a Ticker object using the user's input
+    stock = yf.Ticker(ticker_symbol)
 
-stock_info = bhb_stock.info
-
-#Print one piece of that info: the current market price
-
-print(f"The current market price for BHB is: {stock_info['regularMarketPrice']}")
+    #Get the stock's information
+    stock_info = stock.info
+    #Check if the price data exits BEFORE trying to access it
+    if 'regularMarketPrice' in stock_info and stock_info['regularMarketPrice'] is not None:
+        price = stock_info['regularMarketPrice']
+        #We also use .upper() to make the output look clean
+        print(f"The current market price for {ticker_symbol.upper()} is : {price}")
+    else:
+        #This handles tickers that exist but have no price data
+        print(f"could not find current market price data for '{ticker_symbol.upper()}.'")   
+except Exception as e:
+    #This handles invalid tickers or other errors
+    print(f"An error occurred: {e}")
